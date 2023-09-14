68c2f0536c8412354c35cafe3a1b20143ebbf80a
franciscoryb1 <franciscoryb1@hotmail.com>
2023-09-14 18:31:49 -0300

    primer commit

---
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -0,0 +1,27 @@
+from ..models.grid import Grid
+from ..models.frontier import PriorityQueueFrontier
+from ..models.solution import NoSolution, Solution
+from ..models.node import Node
+
+
+class GreedyBestFirstSearch:
+    @staticmethod
+    def search(grid: Grid) -> Solution:
+        """Find path between two points in a grid using Greedy Best First Search
+
+        Args:
+            grid (Grid): Grid of points
+
+        Returns:
+            Solution: Solution found
+        """
+        # Initialize a node with the initial position
+        node = Node("", grid.start, 0)
+
+        # Initialize the explored dictionary to be empty
+        explored = {} 
+        
+        # Add the node to the explored dictionary
+        explored[node.state] = True
+        
+        return NoSolution(explored)
---
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -0,0 +1,27 @@
+from ..models.grid import Grid
+from ..models.frontier import StackFrontier
+from ..models.solution import NoSolution, Solution
+from ..models.node import Node
+
+
+class UniformCostSearch:
+    @staticmethod
+    def search(grid: Grid) -> Solution:
+        """Find path between two points in a grid using Uniform Cost Search
+
+        Args:
+            grid (Grid): Grid of points
+
+        Returns:
+            Solution: Solution found
+        """
+        # Initialize a node with the initial position
+        node = Node("", grid.start, 0)
+
+        # Initialize the explored dictionary to be empty
+        explored = {} 
+        
+        # Add the node to the explored dictionary
+        explored[node.state] = True
+        
+        return NoSolution(explored)
