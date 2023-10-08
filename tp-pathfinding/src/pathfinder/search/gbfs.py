8b099c03820e7b87d7f912caa3d3ee202aa761bc
C4570 <julianfrancisco2010@gmail.com>
2023-10-08 11:38:38 -0300

    gbfs terminado

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,7 +3,6 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
-
 class GreedyBestFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
@@ -16,12 +15,40 @@ class GreedyBestFirstSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node("", state=grid.start, cost=0, parent=None, action=None)
 
-        # Initialize the explored dictionary to be empty
+        def h(node):
+            return (int(((grid.end[0]-node.state[0])**2 + (grid.end[1]-node.state[1])**2)**(1/2)))
+             
+            
+        
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, h(node))
+        
         explored = {} 
+        explored[node.state] = node.cost
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+        while True:
+
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            node = frontier.pop()
+            successors = grid.get_neighbours(node.state)
+            cost= grid.get_cost(node.state)
+            
+            for action in successors: 
+                
+                new_state = successors[action]
+                if new_state == grid.end:
+                    return Solution(node, explored)
+                
+                new_cost = node.cost + cost
+                
+                if new_state not in explored or new_cost < explored[new_state]:
+                    new_node = Node("", new_state, new_cost, parent=node, action=action)
+                    
+                    explored[new_state] = new_cost
+                    
+                    frontier.add(new_node ,h(new_node))
+
